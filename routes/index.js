88bde7898f580934c0cf85d427e29e5e95ae68a5
nvkex <scrbwrm@gmail.com>
2020-10-02 12:54:18 +0530

    new routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 
 const {
-    checkConnection
+    checkConnection,
+    getURL,
+    shortenURL
 } = require('../controllers/index');
 
 router.get('/', checkConnection);
 
+router.get('/:id', getURL);
+
+router.post('/url', shortenURL);
+
+module.exports = router;
